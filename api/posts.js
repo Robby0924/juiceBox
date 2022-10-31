5df9c5b44421b105d808ae9eb603fd931ba1e61e
Robby Bacus <robbybacus@gmail.com>
2022-10-31 16:38:42 -0400

    continuing POST /api/posts

--- api/posts.js
+++ api/posts.js
@@ -3,9 +3,50 @@ const postsRouter = express.Router();
 const { getAllPosts} = require('../db');
 const { requireUser } = require('./utils');
 
+
+
 postsRouter.post('/', requireUser, async (req, res, next) => {
-  res.send({ message: 'under construction'});
+  const {title, content, tags =""} = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  if (tagArr.length){
+    postData.tags=tagArr;
+  }
+
+  try {
+
+    postData = {
+      authorId,
+      title,
+      content
+    }
+
+    const post = await createPost(postData);
+
+    if (post){
+      res.send ({post})
+    };
+
+  } catch ({ name, message }){
+    next({name, message})
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
+// postsRouter.post('/', requireUser, async (req, res, next) => {
+//   res.send({ message: 'under construction'});
+// });
 
 postsRouter.use((req, res, next) => {
  console.log("A request is being made to /posts");
@@ -21,3 +62,8 @@ postsRouter.get('/', async (req, res) => {
 });
 
 module.exports = postsRouter;
+
+
+
+
+
