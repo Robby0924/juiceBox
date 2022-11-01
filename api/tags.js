8eaf9a5513826986592463f1f6800e50ffae859f
Alex <alexwinston2123@icloud.com>
2022-11-01 13:58:55 -0500

    working filter inactive

--- api/tags.js
+++ api/tags.js
@@ -20,8 +20,11 @@ tagsRouter.get('/:tagName/posts', async (req, res, next) => {
   
   try {
     const posts = await getPostsByTagName(tagName)
-    if (posts){
-      res.send({posts})
+    const filteredPosts = posts.filter(post => {
+      return !post.active || (req.user && post.author.id === !req.user.id);
+    });
+    if (filteredPosts){
+      res.send({filteredPosts})
     }
   } catch ({ name, message }) {
     next({name, message})
